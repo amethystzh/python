ba413195ff8e8c56a6a9659d747aca29240b9be6
AmethystZh <amethystzh@msn.com>
2018-08-06 17:24:35 +0800

    minor

--- webserver1.py
+++ webserver1.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
 
-    HOST, PORT = '', 8888
+    HOST, PORT = '', 8080
 
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -58,7 +58,7 @@ if __name__ == '__main__':
         request = client_connection.recv(1024)
         logging.info('incoming request is: %s' % request)
 
-        http_response = """\
+        http_response = """
         HTTP/1.1 200 OK
     
         Hello, World!
